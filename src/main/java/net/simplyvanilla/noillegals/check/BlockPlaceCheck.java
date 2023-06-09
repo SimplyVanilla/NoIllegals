ebd5b5a6d551d903b479594d2da84e97fcddd177
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-09 09:10:31 +0200

    fix: handle end portal frame activations on block place

--- src/main/java/net/simplyvanilla/noillegals/check/BlockPlaceCheck.java
+++ src/main/java/net/simplyvanilla/noillegals/check/BlockPlaceCheck.java
@@ -10,12 +10,22 @@ import org.bukkit.event.block.BlockPlaceEvent;
 public class BlockPlaceCheck implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
+        // We check if the placed item is a end portal, the block that was placed against is a end portal, and the item in hand is a ender eye.
+        if (event.getBlock().getType().equals(Material.END_PORTAL_FRAME) &&
+            event.getBlockAgainst().getType().equals(
+                Material.END_PORTAL_FRAME) &&
+            event.getItemInHand().getType().equals(Material.ENDER_EYE)) {
+            return;
+        }
         Bukkit.getScheduler().runTaskAsynchronously(NoIllegalsPlugin.getInstance(), () -> {
-            if (NoIllegalsPlugin.checkOPPlayers && event.getPlayer().isOp())
+            if (NoIllegalsPlugin.checkOPPlayers && event.getPlayer().isOp()) {
                 return;
+            }
 
-            if (NoIllegalsPlugin.isItemBlocked(event.getBlock().getType()))
-                Bukkit.getScheduler().runTaskLater(NoIllegalsPlugin.getInstance(), () -> event.getBlock().setType(Material.AIR), 1L);
+            if (NoIllegalsPlugin.isItemBlocked(event.getBlock().getType())) {
+                Bukkit.getScheduler().runTaskLater(NoIllegalsPlugin.getInstance(),
+                    () -> event.getBlock().setType(Material.AIR), 1L);
+            }
         });
     }
 }

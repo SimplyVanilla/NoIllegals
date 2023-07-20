d50cc14bbab25c2739a6ae78d8dce91de1ff87ea
Netherwhal <netherwhal@simplyvanilla.net>
2023-07-20 22:03:37 +0200

    bump 0.2.2 (#17)

--- src/main/java/net/simplyvanilla/noillegals/check/PortalCheck.java
+++ src/main/java/net/simplyvanilla/noillegals/check/PortalCheck.java
@@ -1,28 +1,26 @@
 package net.simplyvanilla.noillegals.check;
 
 import net.simplyvanilla.noillegals.NoIllegalsPlugin;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPortalEnterEvent;
 import org.bukkit.event.entity.EntityPortalEvent;
 
 public class PortalCheck implements Listener {
-  private NoIllegalsPlugin plugin;
+    private final NoIllegalsPlugin plugin;
 
-  public PortalCheck(NoIllegalsPlugin plugin) {
-    this.plugin = plugin;
-  }
-
-  @EventHandler
-  public void handlePortal(EntityPortalEvent event) {
-    if (!this.plugin
-        .getConfig()
-        .getStringList("deniedPortalEntering")
-        .contains(event.getEntityType().name())) {
-      return;
+    public PortalCheck(NoIllegalsPlugin plugin) {
+        this.plugin = plugin;
     }
 
-    event.getEntity().remove();
-  }
+    @EventHandler
+    public void handlePortal(EntityPortalEvent event) {
+        if (!this.plugin
+            .getConfig()
+            .getStringList("deniedPortalEntering")
+            .contains(event.getEntityType().name())) {
+            return;
+        }
+
+        event.getEntity().remove();
+    }
 }

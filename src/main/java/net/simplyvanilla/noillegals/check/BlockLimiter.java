c254894f28428b043b5677d0d63a022af94c3a24
Fulminazzo <fulminazzo21@gmail.com>
2024-03-28 10:08:58 +0100

    Created BlockLimiter with loadBlocks logic

---
+++ src/main/java/net/simplyvanilla/noillegals/check/BlockLimiter.java
@@ -0,0 +1,38 @@
+package net.simplyvanilla.noillegals.check;
+
+import it.unimi.dsi.fastutil.shorts.ShortReferenceImmutablePair;
+import net.simplyvanilla.noillegals.NoIllegalsPlugin;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlockLimiter implements Listener {
+    private final NoIllegalsPlugin plugin;
+    private final Map<Material, Integer> blocks;
+
+    public BlockLimiter(NoIllegalsPlugin plugin) {
+        this.plugin = plugin;
+        this.blocks = new HashMap<>();
+        loadBlocks();
+    }
+
+    private void loadBlocks() {
+        final FileConfiguration configuration = this.plugin.getConfig();
+        final ConfigurationSection limitedBlocks = configuration.getConfigurationSection("limiter.blocks");
+        if (limitedBlocks == null) return;
+
+        for (String raw : limitedBlocks.getKeys(false))
+            try {
+                Material material = Material.valueOf(raw);
+                this.blocks.put(material, limitedBlocks.getInt(raw));
+            } catch (IllegalArgumentException e) {
+                this.plugin.getLogger().warning("Could not find material '%s'".formatted(raw));
+            }
+    })
+}
